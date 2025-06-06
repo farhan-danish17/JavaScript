defc4805b62128948256cfd14605fe66953897a1
Farhan Danish <farhancodes17@gmail.com>
2025-06-06 16:10:32 +0530

    JS11

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -447,7 +447,7 @@ console.log(firstWithdrawal);
 
 console.log(accounts);
 
-const account = accounts.find(acc => acc.owner === 'Jessica Davis');
+const account = accounts.find(acc => acc.owner === 'Akshat');
 console.log(account);
 */
 
@@ -495,4 +495,27 @@ console.log(movements.some(deposit));
 console.log(movements.every(deposit));
 console.log(movements.filter(deposit));
 
+*/
+
+/*
+///////////////////////////////////////
+// flat and flatMap
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+console.log(arr.flat());
+
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+console.log(arrDeep.flat(2));
+
+// flat
+const overalBalance = accounts
+  .map(acc => acc.movements)
+  .flat()
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance);
+
+// flatMap
+const overalBalance2 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance2);
 */
