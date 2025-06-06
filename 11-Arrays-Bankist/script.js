4625339b30262bf884be87d2b27c458638f3726e
Farhan Danish <farhancodes17@gmail.com>
2025-06-06 11:45:19 +0530

    JS11.3.1

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -360,3 +360,23 @@ console.log(avg1, avg2);
 
 */
 
+/*
+//////////////////////////////////////11.3.1
+
+// The Magic of Chaining Methods
+const eurToUsd = 1.1;
+console.log(movements);
+
+// PIPELINE
+const totalDepositsUSD = movements
+  .filter(mov => mov > 0)
+  .map((mov, i, arr) => {
+    // console.log(arr);
+    return mov * eurToUsd;
+  })
+  // .map(mov => mov * eurToUsd)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDepositsUSD);
+*/
+
+
